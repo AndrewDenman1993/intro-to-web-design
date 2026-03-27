ad30dd09f4b393a4715235fc5efd778b4fe5eb16
Andrew Denman <andrewde@uk.ibm.com>
2026-03-27 09:30:40 +0000

    Added bootstrap

--- javascript/site.js
+++ javascript/site.js
@@ -2,3 +2,18 @@
             {
                 document.getElementById("contactForm").reset()
             }
+
+            $(function(){
+var pages=['index','about','contact'];
+var pathname= window.location.pathname;
+
+$('.nav-link').each(function(item){
+    if(pathname.includes(pages[item])){
+        $(this).addClass('active');
+        $(this).attr('aria-current', 'page');
+    }
+
+})
+           
+        
+        });
